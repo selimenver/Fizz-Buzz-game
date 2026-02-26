d013a7dd08a572e6da78a8ce53d8b4e0e73c7f2f
Selim <ebeeeeebebe569@gmail.com>
2026-02-26 15:13:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package main;
+
+class Main{
+    static void fizzbuzz(){
+        for (int i = 1; i <= 100; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz!");
+            }
+            else if (i % 5 == 0){
+                System.out.println("Buzz!");
+            }
+            else if (i % 3 == 0){
+                System.out.println("Fizz!");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+    public static void main(String[] args){
+        fizzbuzz();
+    }
+}
